6a2a7f228440d1a0be90eed09e29fcf314a3e1fc
just1nkorir <justin.korir@student.moringaschool.com>
2023-03-13 14:49:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = 'Leo Messi';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ` + `LEO MESSI` + "!";
+let shortGreeting = "Welcome, L!";
+
